e509c65972d04cb0f1358e1947174c8f7efbd856
UsernameZero <brianessafi@hotmail.co.uk>
2014-06-12 03:39:24 +0100

    Added .addNewArmourRendererPrefix

--- java/gcm/xenorite/proxy/XenoriteClientProxy.java
+++ java/gcm/xenorite/proxy/XenoriteClientProxy.java
@@ -3,5 +3,10 @@ package gcm.xenorite.proxy;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 
 public class XenoriteClientProxy extends XenoriteCommonProxy {
+	
+	@Override
+	public int addArmor(String xenorite) {
+	return RenderingRegistry.addNewArmourRendererPrefix(xenorite);
+	}
 
 }
