6ea98d5bf4d66ac8be01ad7b4e530890673eabcb
BuildTools <unconfigured@null.spigotmc.org>
2015-04-13 13:37:12 +0100

    Adding a way to check for mod update to let peoplez know if there is an update - Credits to VozValden- https://www.youtube.com/watch?v=z8C4mhFAzBo

---
+++ java/gcm/xenorite/handler/EventHandler.java
@@ -0,0 +1,14 @@
+package gcm.xenorite.handler;
+
+import net.minecraft.util.ChatComponentText;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
+
+public class EventHandler {
+	@SubscribeEvent
+	public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
+		if (UpdateHandler.show)
+			event.player.addChatMessage(new ChatComponentText(
+					UpdateHandler.updateStatus));
+	}
+}
