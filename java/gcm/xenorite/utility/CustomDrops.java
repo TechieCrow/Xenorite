b683069022d1e8ab609f4753dce720aef27a97b4
BuildTools <unconfigured@null.spigotmc.org>
2015-04-13 08:42:28 +0100

    Adds Universe Soul Split To global drop table... ANYTHING extending "EntityLivingBase" shoudl now have a 5% chance of dropping 1 Universe Soul Split. (For use taming Xen BEast)

---
+++ java/gcm/xenorite/utility/CustomDrops.java
@@ -0,0 +1,30 @@
+package gcm.xenorite.utility;
+
+import java.util.Random;
+
+import gcm.xenorite.init.ModItems;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+
+public class CustomDrops
+{
+	public static double rand;
+	
+	@SubscribeEvent
+	public void onEntityDrop(LivingDropsEvent event)
+	{
+		if (event.source.getDamageType().equals("player")) //Checks for Damage Type.
+		{
+			rand = Math.random();
+			if (event.entityLiving instanceof EntityLivingBase)
+			{
+				if (rand < 0.05D) //Makes drop 100% drop chance. Example: (0.25D = 25%, 1D = 100%, etc.)
+				{
+					event.entityLiving.dropItem(ModItems.universesoulSplit, 1);
+				}
+			}
+		}
+	}
+}
