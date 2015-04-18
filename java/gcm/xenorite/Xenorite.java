0c4e10b87ff08ef5036be898d915d7c29a1a8758
UsernameZero <techiecrow@gmail.com>
2015-04-18 21:08:37 +0100

    Added Xen Beast's spawn rate, min to spawn and max to spawn to the config file

--- java/gcm/xenorite/Xenorite.java
+++ java/gcm/xenorite/Xenorite.java
@@ -177,7 +177,7 @@ public class Xenorite
 		EntityRegistry.registerGlobalEntityID(entityClass, name, entityID);
 		EntityRegistry.registerModEntity(entityClass, name, entityID, instance, 64, 1, true);
 		EntityList.entityEggs.put(Integer.valueOf(entityID), new EntityList.EntityEggInfo(entityID, primaryColor, secondaryColor)); // probablity of space/minimal to spawn/max to spawn
-		EntityRegistry.addSpawn(XenBeastEntity.class, 10, 1, 5, EnumCreatureType.monster, new BiomeGenBase[]
+		EntityRegistry.addSpawn(XenBeastEntity.class, ConfigurationHandler.xenbeastSpawnRate, ConfigurationHandler.xenbeastMinSpawn, ConfigurationHandler.xenbeastMaxSpawn, EnumCreatureType.monster, new BiomeGenBase[]
 		{ BiomeGenBase.ocean, BiomeGenBase.plains, BiomeGenBase.desert, BiomeGenBase.extremeHills, BiomeGenBase.forest, BiomeGenBase.taiga, BiomeGenBase.swampland, BiomeGenBase.river, BiomeGenBase.frozenOcean, BiomeGenBase.frozenRiver, BiomeGenBase.icePlains, BiomeGenBase.iceMountains, BiomeGenBase.mushroomIsland, BiomeGenBase.mushroomIslandShore, BiomeGenBase.beach, BiomeGenBase.desertHills, BiomeGenBase.forestHills, BiomeGenBase.taigaHills, BiomeGenBase.extremeHillsEdge, BiomeGenBase.jungle, BiomeGenBase.jungleHills, BiomeGenBase.jungleEdge, BiomeGenBase.deepOcean, BiomeGenBase.stoneBeach, BiomeGenBase.coldBeach, BiomeGenBase.birchForest, BiomeGenBase.birchForestHills, BiomeGenBase.roofedForest, BiomeGenBase.coldTaiga, BiomeGenBase.coldTaigaHills, BiomeGenBase.megaTaiga, BiomeGenBase.megaTaigaHills, BiomeGenBase.extremeHillsPlus, BiomeGenBase.savanna, BiomeGenBase.savannaPlateau, BiomeGenBase.mesa, BiomeGenBase.mesaPlateau_F, BiomeGenBase.mesaPlateau });
 	}
 
